3708553bab9a2a3da140dcd9b84e81d78d4f4610
Efrain Segoviano <efrainsegoviano@hotmail.com>
2017-12-11 12:26:39 -0800

    CreditViewModels

---
+++ WebApplication1/WebApplication1/Models/CreditViewModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class CreditViewModels
+    {
+        [Required]
+        [Display(Name = "Nombre de titular")]
+        public string Titular { get; set; }
+
+        [Required]
+        [Display(Name = "CLABE (16 digitos)")]
+        public string Clabe { get; set; }
+
+        [Required]
+        [Display(Name = "Clave (3 digitos)")]
+        public string Clave { get; set; }
+
+        [Required]
+        [Display(Name = "Servicios")]
+        public ICollection<TipoServicio> Servicios { get; set; }
+    }
+}

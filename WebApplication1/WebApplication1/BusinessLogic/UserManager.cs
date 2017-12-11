993565cbd3dff8308e0cfa6d3fdbbf060857ee72
pjf2180 <pjf2180@me.com>
2017-12-11 12:26:23 -0800

    Usuario se agrega  con rol cliente

---
+++ WebApplication1/WebApplication1/BusinessLogic/UserManager.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.BusinessLogic
+{
+    public class ClienteUserManager:CableServicioManager
+    {
+        public void AddClienteUser(Cliente cliente)
+        {
+            try
+            {
+                context.Clientes.Add(cliente);
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
